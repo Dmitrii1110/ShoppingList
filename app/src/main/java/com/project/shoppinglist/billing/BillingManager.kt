91f1334dfb1dbf73d25d2558815562bbb2ec3a32
Dmitrii_IT <74114011+Dmitrii1110@users.noreply.github.com>
2022-01-13 17:51:03 +0300

    Complete session 59(встроенные покупки)

--- app/src/main/java/com/project/shoppinglist/billing/BillingManager.kt
+++ app/src/main/java/com/project/shoppinglist/billing/BillingManager.kt
@@ -6,6 +6,10 @@ import com.android.billingclient.api.*
 class BillingManager(val activity: AppCompatActivity) {
     private var bClient: BillingClient? = null
 
+    init {
+        setUpBillingClient()
+    }
+
     //Функция отключения рекламы
     private fun setUpBillingClient(){
         bClient = BillingClient.newBuilder(activity)
@@ -14,13 +18,52 @@ class BillingManager(val activity: AppCompatActivity) {
             .build()
     }
 
+    // Показ диалога с отображением цены
+    fun startConnection(){
+        bClient?.startConnection(object : BillingClientStateListener{
+            override fun onBillingServiceDisconnected() {
+
+            }
+
+            override fun onBillingSetupFinished(p0: BillingResult) {
+                getItem()
+            }
+
+        })
+    }
+
+    //Детали покупки (что за покупка и тд)
+    private fun getItem(){
+        val skuList = ArrayList<String>()
+        skuList.add(REMOVE_AD_ITEM)
+        val skuDetails = SkuDetailsParams.newBuilder()
+        skuDetails.setSkusList(skuList).setType(BillingClient.SkuType.INAPP)
+        bClient?.querySkuDetailsAsync(skuDetails.build()) {
+                bResult, list ->
+            run {
+                if(bResult.responseCode == BillingClient.BillingResponseCode.OK){
+                    if(list != null) {
+                        if (list.isNotEmpty()){
+                            val bFlowParams = BillingFlowParams
+                                .newBuilder()
+                                .setSkuDetails(list[0]).build()
+                            bClient?.launchBillingFlow(activity, bFlowParams)
+                        }
+                    }
+                }
+            }
+
+        } //Делаем запрос на второстепеном потоке
+    //INAPP это встроенные покупки, не подписка
+    }
+
     //Устанавлвиваем слушатель на покупку отключения рекламы
     private fun getPurchaseListener(): PurchasesUpdatedListener{
         return PurchasesUpdatedListener {
                 bResult, list ->
             run {
                 if (bResult.responseCode == BillingClient.BillingResponseCode.OK) {
-
+                    list?.get(0)?.let { nonConsumableItem(it) }
                 }
             }
         }
@@ -41,5 +84,9 @@ class BillingManager(val activity: AppCompatActivity) {
                 }
             }
         }
+    }
+
+    companion object{
+        const val REMOVE_AD_ITEM = "remove_ad_item_id"
     }
 }
